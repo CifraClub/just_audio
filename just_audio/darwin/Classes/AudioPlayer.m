202b80ae446c57fc36b94ae8a0972ee87a9dd67a
vitorreda <52412187+tivorreda64@users.noreply.github.com>
2024-01-15 10:36:26 -0300

    Testando count > index (#3)

--- just_audio/darwin/Classes/AudioPlayer.m
+++ just_audio/darwin/Classes/AudioPlayer.m
@@ -364,7 +364,7 @@
 - (int)getDuration {
     if (_processingState == none || _processingState == loading) {
         return -1;
-    } else if (_indexedAudioSources && _indexedAudioSources.count > 0) {
+    } else if (_indexedAudioSources && _indexedAudioSources.count > _index) {
         int v = (int)(1000 * CMTimeGetSeconds(_indexedAudioSources[_index].duration));
         return v;
     } else {
